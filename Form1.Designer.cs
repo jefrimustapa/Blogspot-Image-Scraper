af976db7f6eb3067fe267236e00ce36c0bba04da
Juliet Mike <jefri.mustapa@gmail.com>
2020-06-23 17:46:01 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace blogspotImgDownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(196, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 38);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 14;
+            this.listBox1.Location = new System.Drawing.Point(12, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(513, 212);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(456, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(308, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 311);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(537, 24);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.AutoSize = false;
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 18);
+            this.toolStripProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top)
+                        | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right)
+                        | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(210, 19);
+            this.toolStripStatusLabel1.Spring = true;
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top)
+                        | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right)
+                        | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(210, 19);
+            this.toolStripStatusLabel2.Spring = true;
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(421, 258);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 38);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 14);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "blog URL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 335);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Blogspot Image Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+
+// coder : jefri mustapa (https://github.com/jefrimustapa)
+// description : scrape images from specific blogspot based blog 
+/* method: 
+ * - making use of feeds from blogspot to fetch sitemaps. 
+ * - retrieve each blog entries 
+ * - search for all images link
+ * - download image links
+ */
+
+
+namespace blogspotImgDownload
+{
+
+    public partial class Form1 : Form
+    {
+        public Thread thrd = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void updateProgress(int progress)
+        {
+            if (thrd.ThreadState == ThreadState.AbortRequested)
+                return;
+            MethodInvoker action = delegate
+            {
+                toolStripProgressBar1.Value = progress;
+            };
+            this.BeginInvoke(action);
+
+        }
+
+        public void updateListBox(string messageToAdd)
+        {
+            if (thrd.ThreadState == ThreadState.AbortRequested)
+                return;
+            MethodInvoker action = delegate
+            {
+                listBox1.Items.Add(messageToAdd);
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            };
+            this.BeginInvoke(action);
+
+        }
+
+        public void clearListBox()
+        {
+            if (thrd.ThreadState == ThreadState.AbortRequested)
+                return;
+            MethodInvoker action = delegate
+            {
+                listBox1.Items.Clear();
+            };
+            this.BeginInvoke(action);
+
+        }
+
+        public void updateStripLbl1(string str)
+        {
+            if (thrd.ThreadState == ThreadState.AbortRequested)
+                return;
+            MethodInvoker action = delegate
+            {
+                toolStripStatusLabel1.Text = str;
+            };
+            this.BeginInvoke(action);
+
+        }
+
+        public void updateStripLbl2(string str)
+        {
+            if (thrd.ThreadState == ThreadState.AbortRequested)
+                return;
+            MethodInvoker action = delegate
+            {
+                toolStripStatusLabel2.Text = str;
+            };
+            this.BeginInvoke(action);
+
+        }
+
+
+        public void toggleButton()
+        {
+            MethodInvoker action = delegate
+            {
+
+                if (button1.Enabled == true)
+                {
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                }
+                else
+                {
+                    button1.Enabled = true;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                }
+
+
+            };
+
+            this.BeginInvoke(action);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            CDownload dl = new CDownload();
+            dl.url = textBox1.Text;
+
+            dl.fm = this;
+
+            resetStrip();
+            thrd = new Thread(new ThreadStart(dl.download));
+            thrd.Start();
+
+            Application.DoEvents();
+
+
+            return;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (thrd.ThreadState == ThreadState.Suspended)
+            {
+                button2.Text = "Pause";
+                toolStripStatusLabel1.Text = toolStripProgressBar1.Value + "%";
+                thrd.Resume();
+            }
+            else
+            {
+                button2.Text = "Resume";
+                toolStripStatusLabel1.Text = "Paused - " + toolStripProgressBar1.Value + "%";
+                thrd.Suspend();
+            }
+
+        }
+
+
+        private void resizeProgressBar()
+        {
+            toolStripProgressBar1.Width = this.ClientSize.Width - 300;
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            resizeProgressBar();
+        }
+
+
+        private void resetStrip()
+        {
+            toolStripStatusLabel1.Text = "";
+            toolStripStatusLabel2.Text = "";
+            resizeProgressBar();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resetStrip();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (thrd != null)
+                thrd.Abort();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (thrd.ThreadState == ThreadState.Suspended)
+                thrd.Resume();
+            thrd.Abort();
+            resetStrip();
+            updateListBox("Download Aborted By User!");
+            toggleButton();
+        }
+    }
+
+    public class CDownload
+    {
+        public string url;
+        public Form1 fm = null;
+
+
+        public CDownload()
+        {
+
+        }
+
+        public CDownload(string url)
+        {
+            this.url = url;
+        }
+
+        private int processUrl(ref string dir)
+        {
+            int iRet = 0;
+
+            if (url == "")
+            {
+                iRet = -1;
+                goto cleanup;
+            }
+
+            if (url.Substring(0, 7) != "http://")
+                url = "http://" + url;
+
+            url = url.TrimEnd(new char[] { '/' });
+
+            string[] tmp1 = url.Split('/');
+            dir = tmp1[2];
+
+            System.IO.Directory.CreateDirectory(dir);
+
+        cleanup:
+            return iRet;
+        }
+
+        public void crawl()
+        {
+
+        }
+
+        public void download()
+        {
+            string dir = "";
+            string data = "";
+
+            WebClient wc = new WebClient();
+
+            fm.toggleButton();
+            fm.clearListBox();
+
+            int iRet = processUrl(ref dir);
+
+            fm.updateListBox("Fetching sitemap...");
+            try
+            {
+                data = wc.DownloadString(url + "/feeds/posts/default?orderby=UPDATED&max-results=1000000");
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            if (data.Length < 10)
+            {
+                fm.updateListBox("Failed to get sitemap for " + url);
+                fm.toggleButton();
+                return;
+            }
+
+            //fetching the images link
+            fm.updateListBox("Fetching image links...");
+            data = data.Replace("&lt;", "<");
+            data = data.Replace("&gt;", ">");
+            data = data.Replace("&quot;", "'");
+
+
+            List<Uri> links = FetchLinksFromSource(data);
+
+            int progress = 0;
+
+            fm.updateProgress(0);
+            fm.updateListBox("Download Starting...");
+
+            //download images
+
+            for (int i = 0; i < links.Count; i++)
+            {
+                string link = links[i].ToString();
+                string[] tmp = link.Split('/');
+
+                string fname = dir + "/" + i + "_" + tmp[tmp.Length - 1];
+
+                try
+                {
+                    fm.updateListBox("Downloading " + link);
+
+
+                    byte[] dwnByte = wc.DownloadData(link);
+
+                    FileStream fs = new FileStream(fname, FileMode.OpenOrCreate);
+                    BinaryWriter bw = new BinaryWriter(fs);
+
+                    bw.Write(dwnByte);
+
+                    bw.Close();
+                    fs.Close();
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    fm.updateListBox("Failed Download " + fname + ", err:" + ex.Message);
+
+                }
+
+                //update the progress
+                progress = (int)(((double)(i + 1) / (double)(links.Count)) * 100);
+                fm.updateProgress(progress);
+                fm.updateStripLbl1(progress + "%");
+                fm.updateStripLbl2((i + 1) + " of " + links.Count);
+            }
+
+            fm.updateListBox("Download Complete");
+            fm.toggleButton();
+
+        }
+
+        public static List<Uri> FetchLinksFromSource(string htmlSource)
+        {
+
+            List<Uri> links = new List<Uri>();
+            List<string> tmp = new List<string>();
+
+            string regexImgSrc = @"['|""]https?[\S]*\.(jpg|jpeg|png|bmp|gif)['|""]";
+            MatchCollection matchesImgSrc = Regex.Matches(htmlSource, regexImgSrc, RegexOptions.IgnoreCase);
+            foreach (Match m in matchesImgSrc)
+            {
+                tmp.Add(m.Value);
+            }
+
+            // clean links we got
+            for (int i = 0; i < tmp.Count; i++)
+            {
+                string cleanLink = tmp[i].Replace("\'", "").Replace("\"", "");
+                links.Add(new Uri(cleanLink));
+
+            }
+
+            return links;
+        }
+    }
+
+
+
+    public class imageDownloader
+    {
+        public struct _image
+        {
+            string url;
+            bool downloaded;
+        }
+
+        public struct _link
+        {
+            string url;
+            bool crawled;
+            bool downloaded;
+
+        }
+
+        public List<_link> linkList;
+        public List<_image> imageList;
+        public string blogUrl;
+        public int progress;
+
+    }
+
+}
